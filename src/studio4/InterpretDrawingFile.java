39e4ab9463b9494d8b090bbaae23a17f3bd88257
angelinoroibalgiannini <angelinoroibalgiannini@localhost>
2025-06-23 14:58:00 -0700

    commit

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,39 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double pOne = in.nextDouble();
+		double pTwo = in.nextDouble();
+		double pThree = in.nextDouble();
+		double pFour = in.nextDouble();
+		
+		Color color = new Color(r, g, b);
+		StdDraw.setPenColor(color);
+		
+		if (shape.equals("rectangle")) {
+			if (isFilled) {
+				StdDraw.filledRectangle(pOne,pTwo,pThree,pFour);
+			}
+			else {
+				StdDraw.rectangle(pOne,pTwo,pThree,pFour);
+			}
+		}
+		else if (shape.equals("ellipse")) {
+			if (isFilled) {
+				StdDraw.filledEllipse(pOne,pTwo,pThree,pFour);
+			}
+			else {
+				StdDraw.ellipse(pOne,pTwo,pThree,pFour);
+
+			}
+		}
+		
+		
 		
 	}
 }
